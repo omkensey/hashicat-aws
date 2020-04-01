a8a13114ef410c0a42bb5710f421d51140a44464
Joe Thompson <jthompson@hashicorp.com>
2020-04-01 13:41:04 -0400

    Update AZs.

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "gaurav-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-eat-2c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
