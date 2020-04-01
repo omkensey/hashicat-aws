19e8e6cfbe151737337e805f3e5f1a3a5e1cb3ea
Joe Thompson <jthompson@hashicorp.com>
2020-04-01 13:12:58 -0400

    Change placeholder.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  <h1>DOOM KITTENS.</h1>
   <!-- END -->
 
   </div>
